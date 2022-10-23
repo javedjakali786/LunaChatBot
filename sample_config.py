551f3a104987aa2cda38f4845316f7de5056172b
Jack Max <44975340+javedjakali786@users.noreply.github.com>
2022-10-24 02:32:42 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,17 +1,17 @@
-HEROKU = True # Make it False if you're not deploying on heroku.
+HEROKU = False # Make it False if you're not deploying on heroku.
 
 if HEROKU:
     from os import environ
 
-    bot_token = environ["bot_token"]
-    ARQ_API_KEY = environ["ARQ_API_KEY"]
+    bot_token = environ["5707627714:AAFHQWTYxUjM8Ee9XvqmOH-uMIgruuzaO84"]
+    ARQ_API_KEY = environ["LDWNMP-MJMNBL-BNBCHW-UFPYPA-ARQ"]
     LANGUAGE = environ["LANGUAGE"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
 
-    bot_token = "16901971:AAFqdM_SQE1PB2P1xLr67k"
-    ARQ_API_KEY = "Get this from @ARQRobot"
+    bot_token = "5707627714:AAFHQWTYxUjM8Ee9XvqmOH-uMIgruuzaO84"
+    ARQ_API_KEY = "LDWNMP-MJMNBL-BNBCHW-UFPYPA-ARQ"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
     LANGUAGE = "en"
